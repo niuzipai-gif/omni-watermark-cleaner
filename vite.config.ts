6289099866890fb0b96bea0f036a38929029bff1
Administrator <administrator@example.local>
2026-07-27 13:41:33 +0800

    test: exclude isolated worktrees

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
 /// <reference types="vitest" />
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import { configDefaults } from 'vitest/config';
 
 export default defineConfig({
   base: './',
   plugins: [react()],
   test: {
     environment: 'jsdom',
+    exclude: [...configDefaults.exclude, '**/.worktrees/**'],
     setupFiles: ['./src/test/setup.ts']
   },
   build: {
